dfab0b112ed85e2c4eb6006927ed64f4df684f15
Dmitriy <dmitriykaras06@yandex.ru>
2025-06-09 02:11:50 +0300

    l7 final cpp

---
+++ l7/l7/l7/main.cpp
@@ -0,0 +1,66 @@
+﻿#include "trapezoid.h"
+#include <iostream>
+#include <vector>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    vector<trapezoid> trapezoids;
+    int count;
+
+    cout << "Введите количество трапеций: ";
+    cin >> count;
+
+    for (int i = 0; i < count; ++i) {
+        trapezoid t;
+        cout << "Введите координаты трапеции #" << i + 1 << " (x1 y1 x2 y2 base1 base2): ";
+        cin >> t;
+        trapezoids.push_back(t);
+    }
+
+    int choice;
+    bool running = true;
+
+    while (running) {
+        cout << "Меню:" << endl;
+        cout << "1. Вывести все трапеции" << endl;
+        cout << "2. Переместить все трапеции" << endl;
+        cout << "0. Выход" << endl;
+        cout << "Ваш выбор: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            for (size_t i = 0; i < trapezoids.size(); ++i) {
+                cout << "Трапеция #" << i + 1 << ": " << trapezoids[i] << endl;
+                trapezoids[i].printVertices();
+            }
+            break;
+
+        case 2: {
+            double dx, dy;
+            cout << "Введите dx и dy: ";
+            cin >> dx >> dy;
+            for (auto& t : trapezoids) {
+                t.move(dx, dy);
+            }
+            cout << "Трапеции перемещены.\n";
+            break;
+        }
+
+        case 0:
+            running = false;
+            break;
+
+        default:
+            cout << "Неверный выбор. Попробуйте снова.\n";
+            break;
+        }
+    }
+
+    return 0;
+}
---
+++ l7/l7/l7/trapezoid.h
@@ -0,0 +1,50 @@
+#ifndef TRAPEZOID_H
+#define TRAPEZOID_H
+
+#include <iostream>
+
+//класс трапеции
+class trapezoid {
+private:
+	double x1, y1;
+	double x2, y2;
+	double base1, base2;
+
+public:
+	//конструкторы
+	trapezoid(); //по умолчанию
+	trapezoid(double x1_, double y1_, double x2_, double y2_, double b1_, double b2_); //с параметрами
+	trapezoid(const trapezoid& t); //копирования
+
+	~trapezoid(); //деструктор
+
+    //сеттеры
+    void setX1(double val);
+    void setY1(double val);
+    void setX2(double val);
+    void setY2(double val);
+    void setBase1(double val);
+    void setBase2(double val);
+
+    //геттеры
+    double getX1() const;
+    double getY1() const;
+    double getX2() const;
+    double getY2() const;
+    double getBase1() const;
+    double getBase2() const;
+
+    //функции измения положения (move) и вывод значений (printVertices)
+    void move(double dx, double dy);
+    void printVertices() const;
+    
+    //перегрузка операторов + и -
+    trapezoid operator+(const trapezoid& t) const;
+    trapezoid operator-(const trapezoid& t) const;
+
+    //дружные функции
+    friend std::istream& operator>>(std::istream& in, trapezoid& t);
+    friend std::ostream& operator<<(std::ostream& out, const trapezoid& t);
+};
+
+#endif  
